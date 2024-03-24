13b328d91896ae1bf78f79563bf61763d5a0e096
Kunal Kapur <kapur16@scholar-fe01.rcac.purdue.edu>
2024-03-24 12:13:24 -0400

    first version of dcn

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-mport torch
+import torch
 import numpy as np
 from typing import List, cast
 import torch.nn as nn
@@ -10,64 +10,61 @@ class CrossNet(nn.Module):
     Cross Net layer
     """
     def __init__(self, input_dim):
-        R"""
-        image_size: int, size of the input image, For MNIST it is 28
-        output_dim: int, size of the output layer, For MNIST it is 10(equal to number of classes)
-        """
+
         self.alphas = nn.Parameter(torch.Tensor(input_dim, 1, 1))
         self.bias = torch.nn.Parameter(torch.Tensor(1))
+
+        # Kaiming initialization
+        stdv = 1.0/math.sqrt(input_dim)
+        self.alphas.data.uniform_(-stdv, stdv)
+        self.alphas = self.alphas
+        self.bias.data.zero_()
     
     def to(self, device):
         self.alphas = self.alphas.to(device)
         self.bias = self.bias.to(device)
         return super().to(device)
     def forward(self, X):
-        # Input shape: torch.Size([minibatch, input_dim])
-        ####
-        # Task: implement the forward pass
-        # for equivarinat MLP
-        # return the output of the layer 
-        ####
+
         X = torch.mm(X, X.T)
         return nn.functional.linear(X, self.alphas, self.bias)
 
 class DCN(nn.Module):
     R"""
-    image_size: int, size of the input image, For MNIST it is 28
+    input_size: int, size of the input
+    dcn_layer_len: number of cross layers we have
     layer_sizes: List[int], size of the hidden layers
-    output_dim: int, size of the output layer, For MNIST it is 10(equal to number of classes)
+    output_dim: int, size of the output layer
+    embedding_dim: dimension size of the embedding, typically the size of the input dim
     """
-    def __init__(self, input_size, dcn_layer_size, layer_sizes, output_dim):
+    def __init__(self, input_size, dcn_layer_len, layer_sizes, output_dim, embedding_dim=32):
         super().__init__()
-        self.image_size = image_size
+        self.embedding_dim = embedding_dim
+        self.embedding = torch.nn.Embedding(num_embeddings=input_size, embedding_dim=self.embedding_dim)
+
         self.layer_sizes = layer_sizes
         self.output_dim = output_dim
 
-        layers = []
-        ####
-        # Task: initialize the linear layers
-        ###
-        prev_size = self.image_size * self.image_size
-        for i in range(0, len(layer_sizes)):
-            curr_layer = torch.nn.Linear(in_features=prev_size, out_features=layer_sizes[i])
-            layers.append(curr_layer)
-            layers.append(torch.nn.ReLU())
-            prev_size = layer_sizes[i]
-        layers.append(torch.nn.Linear(in_features=prev_size, out_features=output_dim))
-        self.mlp = torch.nn.Sequential(*layers)
+        self.cross_layers = [CrossNet(input_dim=input_size) for i in range(dcn_layer_len)]
 
-    def get_hidden_rep(self, X):
-        hidden_rep = []
-        X = X.reshape(-1, self.image_size * self.image_size)
-        for layer in self.mlp:
-            X = layer(X)
-            hidden_rep.append(X.clone())
-        return hidden_rep
+        prev_dim = layer_sizes[0]
+        self.mlp = []
+        for i in range(1, len(layer_sizes)):
+            self.mlp.append(torch.nn.Linear(in_features=prev_dim, out_features=layer_sizes[i]))
+            self.mlp.append(torch.nn.ReLU())
+
+        # Pop last ReLU layer?
+        self.mlp = torch.nn.Sequential(self.mlp)
+        self.cross_layers = torch.nn.Sequential(self.cross_layers)
+        self.concat_layer = \
+        [torch.nn.Linear(in_features= input_size + layer_sizes[-1], out_features=output_dim), torch.nn.Sigmoid()]
+
+        self.concat_layer = torch.nn.Sequential(self.concat_layer)
 
     def forward(self, X):
-        X = X.reshape(-1, self.image_size * self.image_size)
-        ####
-        # Task: implement the forward pass
-        ###
-        out = self.mlp.forward(X)
-        return out
+        X = self.embedding(X)
+        cross_output = self.cross_layers(X)
+        mlp_output = self.mlp(X)
+        X = torch.concat(cross_output, mlp_output, dim=1)
+        X = self.concat_layer(X)
+        return X

d52fc20bc1499a23b8addc15e75d7fcdf6ec9413
Kunal Kapur <kapur16@scholar-fe05.rcac.purdue.edu>
2024-04-13 13:42:56 -0400

    fixed DCN, added tests to make sure it runs

--- models.py
+++ models.py
@@ -12,33 +12,34 @@ class CrossNet(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
 
-        self.alphas = nn.Parameter(torch.Tensor(1, input_dim, 1))
+        self.alphas = nn.Parameter(torch.Tensor(input_dim, 1))
         self.bias = torch.nn.Parameter(torch.Tensor(input_dim))
 
         # Kaiming initialization
         stdv = 1.0/math.sqrt(input_dim)
         self.alphas.data.uniform_(-stdv, stdv)
         self.alphas = self.alphas
+
         self.bias.data.zero_()
     
     def to(self, device):
         self.alphas = self.alphas.to(device)
         self.bias = self.bias.to(device)
         return super().to(device)
-    def forward(self, X):
+    def forward(self, input, X):
         # Input shape: torch.Size([minibatch, input_dim])
+
         bias = self.bias + X
         X = X.unsqueeze(2)
+        input = input.unsqueeze(2)
 
         transposed_X = torch.transpose(X, 1, 2) # keep batch the same
         X = torch.bmm(X, transposed_X)
-        print("X", X)
-        print("wewight", self.alphas)
-        val = torch.bmm(X, self.alphas.unsqueeze(2)).squeeze(2)
-        print(val)
-        print(bias)
+
+        val = torch.matmul(X, self.alphas).squeeze(2)
+
         return val + bias
-        return nn.functional.linear(X, self.alphas, bias)
+
 
 class DCN(nn.Module):
     R"""
@@ -57,34 +58,34 @@ class DCN(nn.Module):
             nn.Embedding(num_categories, embedding_dim) for num_categories in categorical_features
         ])
 
-        # find sum of total embedding size
-        total_embedding_size = sum(num_categories * embedding_dim for num_categories in categorical_features)
-
         # final input dimension
-        input_dim = total_embedding_size + num_numerical_features
+        input_dim = len(categorical_features)  * embedding_dim + num_numerical_features
 
         self.cross_layers = [CrossNet(input_dim=input_dim) for i in range(dcn_layer_len)]
 
         prev_dim = input_dim
-        self.mlp = []
+        mlp = []
         for i in range(0, len(layer_sizes)):
-            self.mlp.append(torch.nn.Linear(in_features=prev_dim, out_features=layer_sizes[i]))
-            self.mlp.append(torch.nn.ReLU())
+            mlp.append(torch.nn.Linear(in_features=prev_dim, out_features=layer_sizes[i]))
+            mlp.append(torch.nn.ReLU())
+            prev_dim = layer_sizes[i]
 
-        # Pop last ReLU layer?
-        self.mlp = torch.nn.Sequential(self.mlp)
-        self.cross_layers = torch.nn.Sequential(self.cross_layers)
+        mlp.pop() # Pop last ReLU layer?
+        self.mlp = torch.nn.Sequential(*mlp)
 
         # the cross layers have input dim and we add to last layer size
-        self.concat_layer = \
+        concat_layer = \
         [torch.nn.Linear(in_features= input_dim + layer_sizes[-1], out_features=output_dim), torch.nn.Sigmoid()]
 
-        self.concat_layer = torch.nn.Sequential(self.concat_layer)
+        self.concat_layer = torch.nn.Sequential(*concat_layer)
 
     def to(self, device):
 
         for i in range(len(self.embedding_layers)):
             self.embedding_layers[i] = self.embedding_layers[i].to(device)
+
+        for i in range(len(self.cross_layers_layers)):
+            self.cross_layers[i] = self.cross_layers[i].to(device)
         return super.to(device)
     
 
@@ -95,9 +96,15 @@ class DCN(nn.Module):
 
         embedded_categorical = torch.cat(embedded_categorical, dim=1)  # Concatenate along feature dimension
         combined_input = torch.cat([embedded_categorical, numerical_input], dim=1)
+        cross_output = combined_input
 
-        cross_output = self.cross_layers(combined_input)
+
+
+        for cross_layer in self.cross_layers:
+            cross_output = cross_layer(combined_input, cross_output)
+
         mlp_output = self.mlp(combined_input)
-        X = torch.concat(cross_output, mlp_output, dim=1)
+
+        X = torch.concat((cross_output, mlp_output), dim=1)
         X = self.concat_layer(X)
         return X
--- test.py
+++ test.py
@@ -13,12 +13,37 @@ class TestCrossNet(unittest.TestCase):
 
         cross_layer = CrossNet(3)
 
-        cross_layer.alphas = torch.nn.Parameter((torch.ones_like(input) * 2).float())
-        cross_layer.bias = torch.nn.Parameter((torch.ones_like(input)).float())
-        out = cross_layer.forward(input.float())
+        cross_layer.alphas = torch.nn.Parameter((torch.ones(3, 1) * 2).float())
+        cross_layer.bias = torch.nn.Parameter((torch.ones(3)).float())
+        out = cross_layer.forward(input.float(), input.float())
         expected = torch.tensor([14,27,40])
-        print(expected, out)
+
         assert (torch.allclose(out.float(), expected.float()))
+    
+    def test_cross_net_complex(self):
+        torch.manual_seed(1)
+        input = torch.tensor([[1,2,3], [3,2,1]])
+
+        cross_layer = CrossNet(3)
+
+        cross_layer.alphas = torch.nn.Parameter((torch.ones(3, 1) * 2).float())
+        cross_layer.bias = torch.nn.Parameter((torch.ones(3)).float())
+        out = cross_layer.forward(input.float(), input.float())
+        expected = torch.tensor([[14,27,40], [40,27,14]])
+
+        assert (torch.allclose(out.float(), expected.float()))
+
+
+class TestDCN(unittest.TestCase):
+    def test_dcn_basic(self):
+        torch.manual_seed(1)
+
+        model = DCN(categorical_features=[4,5], num_numerical_features=2, embedding_dim=3,
+                    dcn_layer_len=3, layer_sizes=[5,3], output_dim=1)
+
+        cat_input = torch.tensor([[1,2,4,5,8,1]])
+        num_input = torch.tensor([[5,6]])
+        model(cat_input, num_input)
 
 
 if __name__ == '__main__':
